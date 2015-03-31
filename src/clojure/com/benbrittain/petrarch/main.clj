cd555eb0fe6ee570948eb6768ca3a69ed73daab7
Ben Brittain <ben@brittain.org>
2015-03-30 20:58:01 -0400

    add intent start for gps

--- src/clojure/com/benbrittain/petrarch/main.clj
+++ src/clojure/com/benbrittain/petrarch/main.clj
@@ -1,13 +1,30 @@
 (ns com.benbrittain.petrarch.main
     (:require [neko.activity :refer [defactivity set-content-view!]]
               [neko.debug :refer [*a]]
-              [neko.threading :refer [on-ui]]))
+              [neko.notify :refer [toast]]
+              [neko.find-view :refer [find-view]]
+              [neko.threading :refer [on-ui]])
+  (:import com.benbrittain.petrarch.GPSbackground)
+  (:import android.widget.TextView)
+  (:import [android.location LocationManager]
+           [android.content Context]))
 
-(defn transmit-button [i]
-  [:button
-   (assoc button-attributes
-          :text (str i)
-          :on-click (fn [_] (add-symbol i)))]) 
+
+(defn send-data [activity]
+  (let [^TextView input (.getText (find-view activity ::secret-key))]
+    (toast activity
+           (if (empty? input)
+             "Please enter the secret-key"
+             (str "secret key: " input))
+           :long)))
+
+(defn start-background-gps [context activity]
+  (let [^TextView input (.getText (find-view activity ::secret-key))
+        ^LocationManager location-manager (.getSystemService context Context/LOCATION_SERVICE)
+        intent (android.content.Intent.)]
+    (.setClassName intent context "com.benbrittain.petrarch.GPSbackground")
+    (.startService context intent)
+    (toast activity "started GPS" :long)))
 
 (defactivity com.benbrittain.petrarch.MainActivity
   :key :main
@@ -15,5 +32,12 @@
   (fn [this bundle]
     (on-ui
       (set-content-view! (*a)
-        [:linear-layout {}
-         [:text-view {:text "Hello from Clojure!"}]]))))
+                         [:linear-layout {:orientation :vertical
+                                          :layout-width :fill
+                                          :layout-height :wrap}
+                          [:edit-text {:id ::secret-key
+                                       :layout-width :fill}]
+                          [:button {:text "send GPS Data"
+                                    :on-click (fn [_] (send-data (*a)))}]
+                          [:button {:text "Start GPS recording"
+                                    :on-click (fn [_] (start-background-gps (.getApplicationContext this) (*a)))}]]))))
