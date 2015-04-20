7152278d2924ee68a72663cb8a939165d9327527
Ben Brittain <ben@brittain.org>
2015-04-20 13:11:46 -0400

    add password support

--- src/clojure/com/benbrittain/petrarch/main.clj
+++ src/clojure/com/benbrittain/petrarch/main.clj
@@ -9,8 +9,6 @@
   (:import com.benbrittain.petrarch.GPSbackground)
   (:import android.widget.TextView)
   (:import [android.content Context]))
-
-
 ;; GPS Service Control
 
 (defn stop-background-gps [context activity]
@@ -25,14 +23,13 @@
     (.startService context intent)
     (toast activity "Started GPS" :long)))
 
-
 ;; Send Data
 
-
-(defn send-chunk [a-chunk]
-  (let [body (pr-str (assoc {} :coords a-chunk))]
+(defn send-chunk [endpoint secret-key a-chunk]
+  (let [body (pr-str (assoc {} :coords a-chunk
+                               :password (str secret-key)))]
     (def resp
-      (future (client/post "http://10.0.0.6:3000/api/routes/"
+      (future (client/post (str endpoint)
                            {:body body
                             :content-type :edn
                             :socket-timeout 10000
@@ -40,20 +37,20 @@
                             :accept :edn})))))
 
 (defn send-data [context activity]
-  (let [^TextView input (.getText (find-view activity ::secret-key))
-        ; Holy Shit. there has to be a better way.
-        file-name "/data/data/com.benbrittain.petrarch.debug/files/gpsdata.edn"]
+  (let [^TextView secret-key (.getText (find-view activity ::secret-key))
+        ^TextView endpoint (.getText (find-view activity ::endpoint))
+        file-name (str (.dataDir (.getApplicationInfo context)) "/files/gpsdata.edn")]
     (do
       (stop-background-gps context activity)
       (with-open [rdr (clojure.java.io/reader file-name)]
         (doseq [lines (partition-all 50 (line-seq rdr))]
-          (send-chunk (->> lines
-                           (map #(read-string %))))))
-      (start-background-gps context activity)
-      (toast activity
-             "Sent GPS data to server"
-             :long))))
-
+          (send-chunk endpoint secret-key (->> lines
+                                               (map #(read-string %))))))
+      (toast activity "Sent GPS data to server" :long)
+      (io/copy (io/file file-name)
+               (io/file (str file-name ".bk")))
+      (io/delete-file (io/file file-name))
+      (start-background-gps context activity))))
 
 (defactivity com.benbrittain.petrarch.MainActivity
   :key :main
@@ -61,12 +58,17 @@
   (fn [this bundle]
     (on-ui
       (set-content-view! (*a)
-                         [:linear-layout {:orientation :vertical
-                                          :layout-width :fill
-                                          :layout-height :wrap}
-                          [:edit-text {:id ::secret-key
-                                       :layout-width :fill}]
-                          [:button {:text "send GPS Data"
-                                    :on-click (fn [_] (send-data (.getApplicationContext this) (*a)))}]
-                          [:button {:text "Start GPS recording"
-                                    :on-click (fn [_] (start-background-gps (.getApplicationContext this) (*a)))}]]))))
+          [:linear-layout {:orientation :vertical
+                           :layout-width :fill
+                           :layout-height :wrap}
+           [:edit-text {:id ::endpoint
+                        :layout-width :fill
+                        :hint "http://SERVER.com/api/routes"}]
+           [:edit-text {:id ::secret-key
+                        :hint "password"
+                        :layout-width :fill}]
+           [:button {:text "send GPS Data"
+                     :on-click (fn [_] (send-data (.getApplicationContext this) (*a)))}]
+           [:button {:text "Start GPS recording"
+                     :on-click (fn [_] (start-background-gps
+                                         (.getApplicationContext this) (*a)))}]]))))
